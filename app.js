c923c602daca094c16e02a373d45691347904e5f
ankitsinghsinghel <singhelboyankit@gmail.com>
2024-07-21 22:23:01 +0530

    moved

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 
 const express= require('express');
 const app=express();
+const flash= require('connect-flash');
 const mongoose=require('mongoose');
 const cookieParser= require('cookie-parser');
 const session= require('express-session');
-const flash= require('connect-flash');
 const passport= require('passport');
 const LocalStrategy=require('passport-local');
 const User= require('./models/user.js');
